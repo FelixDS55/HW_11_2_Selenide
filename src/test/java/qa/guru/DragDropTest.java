783ccf3e140e2f9ff22b7b09067668ea6ae1e6b9
Mikhail Turilov <turilovm@gmail.com>
2022-12-24 12:24:55 +0300

    add drag_and_drop

--- src/test/java/qa/guru/DragDropTest.java
+++ src/test/java/qa/guru/DragDropTest.java
@@ -1,10 +1,10 @@
 package qa.guru;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DragDropTest {
 
@@ -21,8 +21,11 @@ public class DragDropTest {
     void dragAndDropTestActions(){
         open("https://the-internet.herokuapp.com/drag_and_drop");
         Configuration.holdBrowserOpen = true;
-
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+        actions().dragAndDropBy($("#column-a"), 150, 0).perform();
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(150,0).release().perform();
     }
+
 
 
 }
